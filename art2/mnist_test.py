547224e566823b41a3d0da4a2050d02a3d9d94ed
Aleksander Truszczyński <truszczynskia@student.mini.pw.edu.pl>
2020-11-14 13:06:39 +0100

    MNIST part 1 ready

--- art2/mnist_test.py
+++ art2/mnist_test.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from mnist import MNIST
 from sklearn import preprocessing
 import sklearn.metrics as sm
@@ -29,21 +31,19 @@ onehot_test_labels = enc.transform(test_labels).toarray()
 train_images = train_images/255
 test_images = test_images/255
 
-# test_images = train_images
-# test_labels = train_labels
+test_images = train_images
+test_labels = train_labels
 
-howMany = 1000
+howMany = 3000
 # howMany = len(test_images)
 
 
 test_images = test_images[0:howMany]
 test_labels = test_labels[0:howMany]
 
+net = Art2(28*28, 10, 0.9, 0.001)
 
-
-net = Art2(28*28, 10, 0.8, 0.001)
-
-net.learn(test_images, 5, 2)
+net.learn(test_images, epochs=100, learning_its=5)
 
 conf_mat = np.zeros((10,10))
 
@@ -58,6 +58,51 @@ for i in range(len(np.unique(labels2))):
     print(f'Class {i} - {len(labels2[labels2==i])}')
 print(randScore)
 
-print(conf_mat)
+#print(conf_mat)
 
+# def swapCols(matrix: np.ndarray, col1: int, col2: int):
+#     matrix[:, [col1, col2]] = matrix[:, [col2, col1]]
+#     return matrix
+#
+# def findMaxIndex(matrix: np.ndarray) -> Union[int, int]:
+#     max = -np.Inf
+#     row = -1
+#     col = -1
+#     for r in range(matrix.shape[0]):
+#         for c in range(matrix.shape[1]):
+#             if matrix[r,c] > max:
+#                 max = matrix[r,c]
+#                 row = r
+#                 col = c
+#
+#     return [row, col]
+#
+#
+# def negate(matrix: np.ndarray, row: int, col: int):
+#
+#     for r in range(matrix.shape[0]):
+#         for c in range(matrix.shape[1]):
+#             if r == row or c == col:
+#                 val = matrix[r, c]
+#                 if val > 0:
+#                     matrix[r, c] = -val
+#
+#     return matrix
+#
+# def analyse_cm(matrix: np.ndarray):
+#     cols = matrix.shape[1]
+#     for i in range(cols):
+#         row, col = findMaxIndex(matrix)
+#         if(matrix[row, col] > 0):
+#             matrix = swapCols(matrix, col, row)
+#             matrix = negate(matrix, row, row)
+#     return np.abs(matrix)
+#
+#
+#
+# conf_mat = analyse_cm(conf_mat)
+# print("___")
+# print(conf_mat)
+#
+# print(f'accuracy: {np.sum(np.diag(conf_mat))/np.sum(conf_mat) * 100} %')
 
